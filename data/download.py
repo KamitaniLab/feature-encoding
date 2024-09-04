eebda3cf5824ae03226060e7c3e06e25b42a2462
Shuntaro Aoki <s_aoki@i.kyoto-u.ac.jp>
2024-09-04 11:13:43 +0900

    Update download.py

--- data/download.py
+++ data/download.py
@@ -1,4 +1,5 @@
 import os
+from glob import glob
 import shutil
 import argparse
 import json
@@ -22,13 +23,31 @@ def main(cfg):
 
         # Downloading
         if not os.path.exists(output):
-            print(f'Downloading {output} from {fl["url"]}')
-            download_file(fl['url'], output, progress_bar=True, md5sum=fl['md5sum'])
+            if isinstance(fl['url'], str):
+                print(f'Downloading {output} from {fl["url"]}')
+                download_file(fl['url'], output, progress_bar=True, md5sum=fl['md5sum'])
+            else:
+                # fl['url'] and fl['md5sum'] are lists
+                for i, (url, md5) in enumerate(zip(fl['url'], fl['md5sum'])):
+                    output_chunk = output + f'.{i:04d}'
+                    if os.path.exists(output_chunk):
+                        continue
+                    print(f'Downloading {output_chunk} from {url}')
+                    download_file(url, output_chunk, progress_bar=True, md5sum=md5)
 
         # Postprocessing
         if 'postproc' in fl:
             for pp in fl['postproc']:
-                if pp['name'] == 'unzip':
+                if pp['name'] == 'merge':
+                    if os.path.exists(output):
+                        continue
+                    print(f'Merging {output}')
+                    cat_files = sorted(glob(output + '.*'))
+                    with open(output, 'wb') as f:
+                        for cf in cat_files:
+                            with open(cf, 'rb') as cf_f:
+                                shutil.copyfileobj(cf_f, f)
+                elif pp['name'] == 'unzip':
                     print(f'Unzipping {output}')
                     if 'destination' in pp:
                         dest = pp['destination']
